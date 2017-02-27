a7dbc60e4c95c1915f6f94d9d4c0f028a06a21ea
ingmar <ingmar.stipriaan@holder.nl>
2017-02-27 19:35:22 +0100

    add static files

---
+++ budgetmeester_static/javascripts/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    navigation: true,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    dots: true
+  });
+});
